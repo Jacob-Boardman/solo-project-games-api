bf12417a1fbf1927200e94932f8e39661f296d44
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:03:36 +0000

    Filled out the domains for the API

--- src/main/java/com/qa/persistence/domain/Game.java
+++ src/main/java/com/qa/persistence/domain/Game.java
@@ -1,5 +1,52 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Game {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String title;
+	private Long ageRating;
+	private String gameGenre;
+
+	public Game() {
+		// has to be empty
+	}
+
+	public Game(String title, Long ageRating, String gameGenre) {
+		this.setTitle(title);
+		this.setAgeRating(ageRating);
+		this.setGameGenre(gameGenre);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Long getAgeRating() {
+		return ageRating;
+	}
+
+	public void setAgeRating(Long ageRating) {
+		this.ageRating = ageRating;
+	}
+
+	public String getGamegenre() {
+		return gameGenre;
+	}
+
+	public void setGameGenre(String gameGenre) {
+		this.gameGenre = gameGenre;
+	}
 
 }
--- src/main/java/com/qa/persistence/domain/User.java
+++ src/main/java/com/qa/persistence/domain/User.java
@@ -1,5 +1,63 @@
 package com.qa.persistence.domain;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String firstName;
+	private String secondName;
+	private Long age;
+	private String gameGenre;
+	
+	public User() {
+		//Has to be empty
+	}
+	
+	public User(String firstName, String secondName, Long age, String gameGenre) {
+		this.setFirstName(firstName);
+		this.setSecondName(secondName);
+		this.setAge(age);
+		this.setGameGenre(gameGenre);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+	public String getGameGenre() {
+		return gameGenre;
+	}
+
+	public void setGameGenre(String gameGenre) {
+		this.gameGenre = gameGenre;
+	}
+	
+	
 }
