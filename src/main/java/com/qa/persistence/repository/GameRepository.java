ac10edbd039c48ccf170a0f73e91d8fa39de4588
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:11:35 +0000

    Added game Interface methods

--- src/main/java/com/qa/persistence/repository/GameRepository.java
+++ src/main/java/com/qa/persistence/repository/GameRepository.java
@@ -1,5 +1,15 @@
 package com.qa.persistence.repository;
 
 public interface GameRepository {
+	
+	String createGame(String game);
+	
+	String getAllGames();
+	
+	String getGameById(Long id);
+	
+	String updateGame(Long id, String game);
+	
+	String deleteGame(Long id);
 
 }
