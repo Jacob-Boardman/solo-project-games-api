eba76f61225b1bf6c17d522bdc76ea3107d92e7d
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:18:06 +0000

    Added in JSONUtil and the ground work for the game DB Repo

--- src/main/java/com/qa/persistence/repository/GameDBRepository.java
+++ src/main/java/com/qa/persistence/repository/GameDBRepository.java
@@ -1,7 +1,27 @@
 package com.qa.persistence.repository;
 
-public class GameDBRepository implements GameRepository{
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+import static javax.transaction.Transactional.TxType.REQUIRED;
 
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.qa.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class GameDBRepository implements GameRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
+
+	@Transactional(REQUIRED)
 	public String createGame(String game) {
 		// TODO Auto-generated method stub
 		return null;
@@ -17,14 +37,28 @@ public class GameDBRepository implements GameRepository{
 		return null;
 	}
 
+	@Transactional(REQUIRED)
 	public String updateGame(Long id, String game) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional(REQUIRED)
 	public String deleteGame(Long id) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public void findGame(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	public EntityManager getManager() {
+		return manager;
+	}
+
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
 	}
 
 }
