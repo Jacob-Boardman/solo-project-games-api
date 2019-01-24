3790df15fff0bff5e3acc765d45a1b3b121603d5
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:29:42 +0000

    Added in game DB functionality

--- src/main/java/com/qa/persistence/repository/GameDBRepository.java
+++ src/main/java/com/qa/persistence/repository/GameDBRepository.java
@@ -1,14 +1,19 @@
 package com.qa.persistence.repository;
 
 import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
 import static javax.transaction.Transactional.TxType.REQUIRED;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 
+import com.qa.persistence.domain.Game;
 import com.qa.util.JSONUtil;
 
 @Transactional(SUPPORTS)
@@ -23,34 +28,49 @@ public class GameDBRepository implements GameRepository {
 
 	@Transactional(REQUIRED)
 	public String createGame(String game) {
-		// TODO Auto-generated method stub
-		return null;
+		Game newGame = util.getObjectForJSON(game, Game.class);
+		manager.persist(newGame);;
+		return "Message : Created a new Game";
 	}
 
 	public String getAllGames() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT g FROM Game g");
+		Collection<Game> games = (Collection<Game>)query.getResultList();
+		return util.getJSONForObject(games);
 	}
 
 	public String getGameById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT g FROM Game g WHERE id = id");
+		Collection<Game> games = (Collection<Game>)query.getResultList();
+		return util.getJSONForObject(games);
 	}
 
 	@Transactional(REQUIRED)
 	public String updateGame(Long id, String game) {
-		// TODO Auto-generated method stub
-		return null;
+		Game foundGame = findGame(id);
+		Game newGame = util.getObjectForJSON(game, Game.class);
+		if(foundGame != null)
+		{
+			manager.remove(foundGame);
+			manager.persist(newGame);
+			return "Message: Game has been updated";
+		}
+		return "Message : No Game found";
 	}
 
 	@Transactional(REQUIRED)
 	public String deleteGame(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Game foundGame = findGame(id);
+		if(foundGame != null)
+		{
+			manager.remove(foundGame);
+			return "Message: Game has been deleted";
+		}
+		return "Message : No Game found";
 	}
 
-	public void findGame(EntityManager manager) {
-		this.manager = manager;
+	public Game findGame(Long id) {
+		return manager.find(Game.class, id);
 	}
 
 	public EntityManager getManager() {
