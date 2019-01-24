b641d6f9ae1b1341020fa2fbff5973eb615901af
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:40:49 +0000

    Added in unimplemented methods

--- src/main/java/com/qa/business/service/GameServiceImp.java
+++ src/main/java/com/qa/business/service/GameServiceImp.java
@@ -2,4 +2,29 @@ package com.qa.business.service;
 
 public class GameServiceImp implements GameService{
 
+	public String createGame(String game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getGameById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateGame(Long id, String game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteGame(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/com/qa/business/service/UserServiceImp.java
+++ src/main/java/com/qa/business/service/UserServiceImp.java
@@ -2,4 +2,29 @@ package com.qa.business.service;
 
 public class UserServiceImp implements UserService{
 
+	public String createUser(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUserById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateUser(Long id, String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
