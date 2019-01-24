3be4f08512fe250923a06b24f535ed86fb4b5063
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:12:22 +0000

    Added in unimplemented methods to the DB files

--- src/main/java/com/qa/persistence/repository/GameDBRepository.java
+++ src/main/java/com/qa/persistence/repository/GameDBRepository.java
@@ -2,4 +2,29 @@ package com.qa.persistence.repository;
 
 public class GameDBRepository implements GameRepository{
 
+	public String createGame(String game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getGameById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateGame(Long id, String game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteGame(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/com/qa/persistence/repository/UserDBRepository.java
+++ src/main/java/com/qa/persistence/repository/UserDBRepository.java
@@ -2,4 +2,29 @@ package com.qa.persistence.repository;
 
 public class UserDBRepository implements UserRepository{
 
+	public String createUser(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUserById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateUser(Long id, String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
