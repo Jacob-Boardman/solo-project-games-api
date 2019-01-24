9ec9225e440928cced894774ff6d80a6eaa787ec
Your Name <jacob.baordman.jb@gmail.com>
2019-01-24 16:38:04 +0000

    Added in User DB repo functionality

--- src/main/java/com/qa/persistence/repository/UserDBRepository.java
+++ src/main/java/com/qa/persistence/repository/UserDBRepository.java
@@ -1,30 +1,83 @@
 package com.qa.persistence.repository;
 
-public class UserDBRepository implements UserRepository{
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+import static javax.transaction.Transactional.TxType.REQUIRED;
 
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.qa.persistence.domain.Game;
+import com.qa.persistence.domain.User;
+import com.qa.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
+public class UserDBRepository implements UserRepository {
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
+
+	@Transactional(REQUIRED)
 	public String createUser(String user) {
-		// TODO Auto-generated method stub
-		return null;
+		Game newUser = util.getObjectForJSON(user, Game.class);
+		manager.persist(newUser);
+		return "Message : Created a new User";
 	}
 
 	public String getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT u FROM User u");
+		Collection<User> users = (Collection<User>) query.getResultList();
+		return util.getJSONForObject(users);
 	}
 
 	public String getUserById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT u FROM User u WHERE id = id");
+		Collection<User> users = (Collection<User>) query.getResultList();
+		return util.getJSONForObject(users);
 	}
 
+	@Transactional(REQUIRED)
 	public String updateUser(Long id, String user) {
-		// TODO Auto-generated method stub
-		return null;
+		User foundUser = findUser(id);
+		Game newUser = util.getObjectForJSON(user, Game.class);
+		if (foundUser != null) {
+			manager.remove(foundUser);
+			manager.persist(newUser);
+			return "Message : User has been updated";
+		}
+		return "Message : No user found";
 	}
 
+	@Transactional(REQUIRED)
 	public String deleteUser(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		User foundUser = findUser(id);
+		if (foundUser != null) {
+			manager.remove(foundUser);
+
+			return "Message : User has been deleted";
+		}
+		return "Message : No user found";
+	}
+
+	public User findUser(Long id) {
+		return manager.find(User.class, id);
+	}
+
+	public EntityManager getManager() {
+		return manager;
+	}
+
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
 	}
 
 }
