075306ebbde6d887626e5f85238c4865bae35614
Your Name <jacob.baordman.jb@gmail.com>
2019-02-01 09:36:01 +0000

    Implemented get games by genre

--- src/main/java/com/qa/persistence/repository/GameDBRepository.java
+++ src/main/java/com/qa/persistence/repository/GameDBRepository.java
@@ -70,6 +70,12 @@ public class GameDBRepository implements GameRepository {
 		return "Message : No Game found";
 	}
 
+	public String getGamesBygenre(String genre) {
+		Query query = manager.createQuery("SELECT g FROM Game g WHERE gameGenre = " + genre);
+		Collection<Game> games = (Collection<Game>) query.getResultList();
+		return util.getJSONForObject(games);
+	}
+	
 	public Game findGame(Long id) {
 		return manager.find(Game.class, id);
 	}
@@ -81,5 +87,6 @@ public class GameDBRepository implements GameRepository {
 	public void setManager(EntityManager manager) {
 		this.manager = manager;
 	}
+
 
 }
